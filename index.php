48e7e28dc091b8f728f3b288c332838933bb84ec
g4merka <40857098+g4merka@users.noreply.github.com>
2020-11-29 13:15:42 +0500

    Update index.php

--- index.php
+++ index.php
@@ -5,13 +5,10 @@
   <body>
   <p>Дьяконов Юрий ПИ-320 </p>
   <p>Дата и время:</p>
-    <?php 
-    $d=date("d.m.Y. H:i");
-    echo ($d); 
-    ?> 
+
     <h3>Лабораторная работа 1</h3>
     <ul>
-     <li><a href="https://lr1.g4merka.repl.co/lab1-5.php">Задача 1-1</a></li>
+     <li><a href="https://g4merka.github.io/lab1-5.php">Задача 1-1</a></li>
 
     </ul>
   </body>
