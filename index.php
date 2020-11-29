1557a901ab554478c2fc6027e92bfc809c23e9cd
g4merka <40857098+g4merka@users.noreply.github.com>
2020-11-29 13:12:05 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    <title>Дьяконов Юрий</title>
+  </head>
+  <body>
+  <p>Дьяконов Юрий ПИ-320 </p>
+  <p>Дата и время:</p>
+    <?php 
+    $d=date("d.m.Y. H:i");
+    echo ($d); 
+    ?> 
+    <h3>Лабораторная работа 1</h3>
+    <ul>
+     <li><a href="https://lr1.g4merka.repl.co/lab1-5.php">Задача 1-1</a></li>
+
+    </ul>
+  </body>
+</html>
